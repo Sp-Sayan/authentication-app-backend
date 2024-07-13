76857804c5366bb98e6c42ce728c69ad9b047e93
Sayan Paul <131338948+Sp-Sayan@users.noreply.github.com>
2024-07-13 21:14:53 +0530

    Update server.js

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ mongoose.connect(mongourl).then(() => {
 const app = express();
 app.use(express.json());
 app.use(cors({
-    origin: https://portal-app-self.vercel.app/,
+    origin: "https://portal-app-self.vercel.app/",
     credentials: true 
 }));
 
